89dfe5c29e5dd38835f53f4879d0afdfdc4c33bb
Windsmasher <paczka94@gmail.com>
2026-04-30 11:00:02 +0200

    feat(main): about me in ghost button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,4 @@
 import Link from 'next/link'
-import IconInstagram from './icons/IconInstagram'
-import IconFacebook from './icons/IconFacebook'
-
-const nav = [
-  { href: '/about-me', label: 'O mnie' },
-] as const
 
 export default function Header() {
   return (
@@ -15,35 +9,13 @@ export default function Header() {
       <div className="header__right">
         <nav className="header__nav" aria-label="Nawigacja główna">
           <ul className="header__nav-list">
-            {nav.map((item) => (
-              <li key={item.href}>
-                <Link href={item.href} className="header__link">
-                  {item.label}
-                </Link>
-              </li>
-            ))}
+            <li>
+              <Link href="/about-me" className="header__link header__link--outline">
+                O mnie
+              </Link>
+            </li>
           </ul>
         </nav>
-        <div className="header__socials" aria-label="Social media">
-          <a
-            href="https://www.instagram.com/justyna_paczka"
-            className="header__social-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram"
-          >
-            <IconInstagram className="header__social-icon" />
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=100057305956163"
-            className="header__social-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Facebook"
-          >
-            <IconFacebook className="header__social-icon" />
-          </a>
-        </div>
         <a
           href="https://booksy.com/pl-pl/dl/show-business/298223"
           className="header__cta"
